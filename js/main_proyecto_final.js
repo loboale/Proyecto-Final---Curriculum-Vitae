e745baa3a71e7269ec1186b2d42306f3fd9620f5
Alexis Aragon Rodriguez <alexisaragon79@gmail.com>
2023-01-17 16:37:21 -0300

    Segunda esvccritura en el repositorio

---
+++ js/main_proyecto_final.js
@@ -0,0 +1,25 @@
+//configuracion de botones
+
+document.getElementById("boton_celeste").addEventListener("click", function (){
+    document.body.style.backgroundColor = "#DEE7E6"
+});
+
+document.getElementById("boton_gris").addEventListener("click", function (){
+    document.body.style.backgroundColor = "#C2CFCF"
+});
+
+document.getElementById("boton_rojo").addEventListener("click", function (){
+    document.body.style.backgroundColor = "#E43E68"
+});
+
+document.getElementById("boton_verde").addEventListener("click", function (){
+    document.body.style.backgroundColor = "rgb(181, 224, 184)"
+});
+
+document.getElementById("boton_amarillo").addEventListener("click", function (){
+    document.body.style.backgroundColor = "#FFE55C"
+});
+
+document.getElementById("boton_blanco").addEventListener("click", function (){
+    document.body.style.backgroundColor = "white"
+});
